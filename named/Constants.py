e85358e802bc4932d208f319a5d84c28fa05fca0
HausReport <tj_willis@hotmail.com>
2020-05-20 13:48:20 -0400

    Create Constants.py

---
+++ named/Constants.py
@@ -0,0 +1,20 @@
+#
+# Faction constants
+#
+GOVERNMENT = 'government'
+MINOR_FACTION_ID = 'minor_faction_id'
+MINOR_FACTION_PRESENCES = 'minor_faction_presences'
+NEIGHBOR_COUNT = 'neighbor_count'
+HAS_ANARCHY = 'hasAnarchy'
+ID = 'id'
+NEIGHBORS = 'neighbors'
+POWER_STATE = 'power_state'
+
+#
+# System constants
+#
+HAS_DOCKING = 'has_docking'
+SYSTEM_ID = 'system_id'
+PAD_SIZE = 'max_landing_pad_size'
+DISTANCE_TO_STAR = 'distance_to_star'
+STATION_TYPE = 'type'
