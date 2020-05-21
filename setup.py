d8f77d30fb2a12a34904704b79bceeff7d5ae14e
HausReport <tj_willis@hotmail.com>
2020-05-21 15:10:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='clubraiders',
+    name='craid',
     url='https://github.com/HausReport/ClubRaiders',
     author='Erlaed',
     author_email='Erlaed2@fdev.not',
     # Needed to actually package something
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    #packages=['craid'],
+    #packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    packages=['craid'],
     # Needed for dependencies
     #install_requires=['numpy'],
     # *strongly* suggested for sharing
-    version='0.25',
+    version='0.26',
     # The license can be anything you like
     license='MIT',
     description='Placeholder description',
