d868ce38c380d9e8ed3fd07d8072ba1bc08f17d6
HausReport <tj_willis@hotmail.com>
2020-05-21 04:37:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='Measurements',
-    url='https://github.com/jladan/package_demo',
-    author='John Ladan',
-    author_email='jladan@uwaterloo.ca',
+    url='https://github.com/HausReport/ClubRaiders',
+    author='Erlaed',
+    author_email='Erlaed@fdev.not',
     # Needed to actually package something
-    packages=['measure'],
+    packages=['named'],
     # Needed for dependencies
-    install_requires=['numpy'],
+    #install_requires=['numpy'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
