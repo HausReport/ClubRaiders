8cd2157abc6f0216aa86b3daa2873d936ea86c26
HausReport <tj_willis@hotmail.com>
2020-05-21 15:22:57 -0400

    fixing structure

--- craid/LoadDataFromEDDB.py
+++ craid/LoadDataFromEDDB.py
@@ -1,20 +1,23 @@
-import requests
 import os
-if not os.path.exists('data'):
-    os.makedirs('data')
 
-if(True is True):
-  #
-  # Gets the most recent data dumps from eddb.io
-  #
-  url = 'https://eddb.io/archive/v6/systems_populated.jsonl'
-  r = requests.get(url, allow_redirects=True)
-  open('data/systems_populated.jsonl', 'wb').write(r.content)
+import requests
 
-  #url = 'https://eddb.io/archive/v6/stations.jsonl'
-  #r = requests.get(url, allow_redirects=True)
-  #open('data/stations.jsonl', 'wb').write(r.content)
 
-  url = 'https://eddb.io/archive/v6/factions.jsonl'
-  r = requests.get(url, allow_redirects=True)
-  open('data/factions.jsonl', 'wb').write(r.content)
+def load_data():
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    if (True is True):
+        #
+        # Gets the most recent data dumps from eddb.io
+        #
+        url = 'https://eddb.io/archive/v6/systems_populated.jsonl'
+        r = requests.get(url, allow_redirects=True)
+        open('data/systems_populated.jsonl', 'wb').write(r.content)
+
+        # url = 'https://eddb.io/archive/v6/stations.jsonl'
+        # r = requests.get(url, allow_redirects=True)
+        # open('data/stations.jsonl', 'wb').write(r.content)
+
+        url = 'https://eddb.io/archive/v6/factions.jsonl'
+        r = requests.get(url, allow_redirects=True)
+        open('data/factions.jsonl', 'wb').write(r.content)
