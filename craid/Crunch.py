a303a718aee597c71b7b04696d9051053913b114
HausReport <tj_willis@hotmail.com>
2020-05-21 15:55:45 -0400

    fixing structure

--- craid/Crunch.py
+++ craid/Crunch.py
@@ -1,45 +1,11 @@
 import json
-
 import pandas as pd
-
 from craid import Club
 from craid.eddb.Faction import Faction
 from craid.eddb.FactionInstance import FactionInstance
 from craid.eddb.InhabitedSystem import InhabitedSystem
 
-all_factions_dict: Faction = {}
 # all_stations_dict : Station = {}
-all_systems_dict: InhabitedSystem = {}
-club_factions_dict: Faction = {}
-club_systems_arr: FactionInstance = [ ]
-
-#
-# Identified Pro-Club Faction Names
-#
-
-
-with open("data/factions.jsonl", 'r') as handle:
-    for line in handle:
-        lCurFaction = json.loads(line)
-        lCurFactionId = int(lCurFaction[ 'id' ])
-        curFaction = Faction(lCurFaction)
-        all_factions_dict[ lCurFactionId ] = curFaction
-        if Club.proClubFaction(curFaction):
-            club_factions_dict[ lCurFactionId ] = curFaction
-
-# with open(stations_file, 'r') as handle:
-#      for line in handle:
-#         station = json.loads(line)
-#         system_id = station[ SYSTEM_ID ]
-#         current_station = stations_dict.get(system_id)
-
-with open("data/systems_populated.jsonl", 'r') as handle:
-    for line in handle:
-        sys = json.loads(line)
-        tid = int(sys[ 'id' ])
-        foo = InhabitedSystem(sys)
-        all_systems_dict[ tid ] = foo
-
 
 def desiredState(state_dict):
     for state in state_dict:
@@ -52,85 +18,117 @@ def desiredState(state_dict):
     return 0
 
 
-for tsys in all_systems_dict.values():
-    ttsys: InhabitedSystem = tsys
-    mfp = ttsys.getMinorFactionPresences()
-    for faction_ptr in mfp:
-        if faction_ptr is None:
-            continue
-        faction_id = int(faction_ptr[ 'minor_faction_id' ])
-        if faction_id is None:
-            continue
-        if faction_id in club_factions_dict:
-            fac = club_factions_dict[ faction_id ]
-            facname = fac.get_name2()
-            if (facname.startswith("*")): continue  # filters player factions
+def getSystemsArray():
+    #all_factions_dict: Faction = {}            #private
+    club_factions_dict: Faction = {}           #private
+    all_systems_dict: InhabitedSystem = {}     #private
+    club_systems_arr: FactionInstance = [ ]    #make this one avaiable
+    with open("data/factions.jsonl", 'r') as handle:
+        for line in handle:
+            lCurFaction = json.loads(line)
+            lCurFactionId = int(lCurFaction[ 'id' ])
+            curFaction = Faction(lCurFaction)
+            #all_factions_dict[ lCurFactionId ] = curFaction
+            if Club.proClubFaction(curFaction):
+                club_factions_dict[ lCurFactionId ] = curFaction
 
-            sysname = ttsys.get_name()
-            facHome = fac.get_homesystem_id()
-            sysid = ttsys.get_id()
-            vulnerable = True
-            if (sysid == facHome): vulnerable = False
+    # with open(stations_file, 'r') as handle:
+    #      for line in handle:
+    #         station = json.loads(line)
+    #         system_id = station[ SYSTEM_ID ]
+    #         current_station = stations_dict.get(system_id)
 
-            govt = ttsys.getGovernment()
-            # allg = fac.get_allegiance()
+    with open("data/systems_populated.jsonl", 'r') as handle:
+        for line in handle:
+            sys = json.loads(line)
+            tid = int(sys[ 'id' ])
+            foo = InhabitedSystem(sys)
+            all_systems_dict[ tid ] = foo
 
-            inf = faction_ptr[ 'influence' ]
-            # sinf = '{:04.2f}'.format(inf)
-            # print(sinf)
 
-            # updated = ttsys.getUpdated();
-            # date = datetime.datetime.utcfromtimestamp(updated)
-            # ds = date.strftime("%m/%d/%Y %H:%M:%S")
 
-            active_states = json.dumps(faction_ptr[ 'active_states' ])
-            hasWar = desiredState(faction_ptr[ 'active_states' ])
-            if (govt == "Anarchy"):
-                hasWar = -16
-            if (hasWar == 0 and inf <= 3.5 and inf > 0.0):
-                hasWar = -15
-            if (hasWar == 104 and inf > 10.0):
-                hasWar = 0
+    for tsys in all_systems_dict.values():
+        ttsys: InhabitedSystem = tsys
+        mfp = ttsys.getMinorFactionPresences()
+        for faction_ptr in mfp:
+            if faction_ptr is None:
+                continue
+            faction_id = int(faction_ptr[ 'minor_faction_id' ])
+            if faction_id is None:
+                continue
+            if faction_id in club_factions_dict:
+                fac = club_factions_dict[ faction_id ]
+                facname = fac.get_name2()
+                if (facname.startswith("*")): continue  # filters player factions
 
-            sysIns = FactionInstance(fac, ttsys, inf, hasWar)
-            club_systems_arr.append(sysIns)
-            # print(facname + "," + sysname)
-            # print("=====================================================================================")
-            # print(facname + "," + sysname + "," + x + "," + y + "," + z + "," + allg + "," + sinf + "," + war+ "," + ds )  # + "," + allg)
+                sysname = ttsys.get_name()
+                facHome = fac.get_homesystem_id()
+                sysid = ttsys.get_id()
+                vulnerable = True
+                if (sysid == facHome): vulnerable = False
 
-xs = [ ]
-ys = [ ]
-zs = [ ]
-factionName = [ ]
-systemName = [ ]
-allgs = [ ]
+                govt = ttsys.getGovernment()
+                # allg = fac.get_allegiance()
 
-for xcs in club_systems_arr:
-    cs: FactionInstance = xcs
-    # vulnerable = cs.isVulnerable()
-    # if (vulnerable == False): continue
-    factionName.append(cs.get_name())
-    systemName.append(cs.getSystemName())
-    xs.append(cs.getX())
-    ys.append(cs.getY())
-    zs.append(cs.getZ())
-    allgs.append(cs.get_allegiance())
+                inf = faction_ptr[ 'influence' ]
+                # sinf = '{:04.2f}'.format(inf)
+                # print(sinf)
 
-data = {
-    'systemName': systemName,
-    'factionName': factionName,
-    'x': xs,
-    'y': ys,
-    'z': zs,
-    'allegiance': allgs
-}
+                # updated = ttsys.getUpdated();
+                # date = datetime.datetime.utcfromtimestamp(updated)
+                # ds = date.strftime("%m/%d/%Y %H:%M:%S")
 
-#
-# Main dataframe of all club factions
-#
-df = pd.DataFrame(data=data)
+                active_states = json.dumps(faction_ptr[ 'active_states' ])
+                hasWar = desiredState(faction_ptr[ 'active_states' ])
+                if (govt == "Anarchy"):
+                    hasWar = -16
+                if (hasWar == 0 and inf <= 3.5 and inf > 0.0):
+                    hasWar = -15
+                if (hasWar == 104 and inf > 10.0):
+                    hasWar = 0
 
-#
-# Dataframe of all club factions except Emp Grace
-#
-filter = df[ ~df[ 'factionName' ].str.contains("Emperor's Grace") ]
+                sysIns = FactionInstance(fac, ttsys, inf, hasWar)
+                club_systems_arr.append(sysIns)
+                # print(facname + "," + sysname)
+                # print("=====================================================================================")
+                # print(facname + "," + sysname + "," + x + "," + y + "," + z + "," + allg + "," + sinf + "," + war+ "," + ds )  # + "," + allg)
+
+    return club_systems_arr
+
+#=========================================================!!!!!!!!!!!!!!!
+def getDataFrame(csa):
+
+    xs = [ ]
+    ys = [ ]
+    zs = [ ]
+    factionName = [ ]
+    systemName = [ ]
+    allgs = [ ]
+
+    for xcs in csa:
+        cs: FactionInstance = xcs
+        # vulnerable = cs.isVulnerable()
+        # if (vulnerable == False): continue
+        factionName.append(cs.get_name())
+        systemName.append(cs.getSystemName())
+        xs.append(cs.getX())
+        ys.append(cs.getY())
+        zs.append(cs.getZ())
+        allgs.append(cs.get_allegiance())
+
+    data = {
+        'systemName': systemName,
+        'factionName': factionName,
+        'x': xs,
+        'y': ys,
+        'z': zs,
+        'allegiance': allgs
+    }
+
+    #
+    # Main dataframe of all club factions
+    #
+    df = pd.DataFrame(data=data)
+    return df
+
+
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Needed for dependencies
     #install_requires=['numpy'],
     # *strongly* suggested for sharing
-    version='0.28',
+    version='0.30',
     # The license can be anything you like
     license='MIT',
     description='Placeholder description',
