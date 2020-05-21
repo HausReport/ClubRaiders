4473480ff90170489be9bb38b1dec87e87f24cae
HausReport <tj_willis@hotmail.com>
2020-05-21 13:52:18 -0400

    fixing structure

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-from setuptools import setup
+import io
+import os
+import sys
+from shutil import rmtree
 
+from setuptools import find_packages, setup, Command
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='Named',
@@ -7,11 +11,12 @@ setup(
     author='Erlaed',
     author_email='Erlaed2@fdev.not',
     # Needed to actually package something
-    packages=['craid'],
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    #packages=['craid'],
     # Needed for dependencies
     #install_requires=['numpy'],
     # *strongly* suggested for sharing
-    version='0.22',
+    version='0.23',
     # The license can be anything you like
     license='MIT',
     description='Placeholder description',
