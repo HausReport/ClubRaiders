d8b08127f67634e397ecf9e53a867dd78aea7c34
HausReport <tj_willis@hotmail.com>
2020-05-24 03:50:28 -0400

    incremental

--- craid/dash/app.py
+++ craid/dash/app.py
@@ -1,3 +1,5 @@
+from typing import Dict, Tuple
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -7,84 +9,107 @@ from dash.dependencies import Input, Output
 
 import craid.eddb.DataProducer as dp
 
-#df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+# df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
 
 arrs = dp.getDataArrays()
-csa = arrs[0]
+csa = arrs[ 0 ]
+systems: Dict[ str, Tuple[ float, float, float ] ] = arrs[2]
 df = dp.getDataFrame(csa)
 
-
-#@font-face {
-  #font-family: myFirstFont;
-  #src: url(sansation_light.woff);
-#}
+# @font-face {
+# font-family: myFirstFont;
+# src: url(sansation_light.woff);
+# }
 #
-#div {
-  #font-family: myFirstFont;
-#}
+# div {
+# font-family: myFirstFont;
+# }
 
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
 
-def generate_table(dataframe, max_rows=10):
+
+def generate_table(dataframe: pd.DataFrame, max_rows: int = 10):
     return html.Table([
         html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
+            html.Tr([ html.Th(col) for col in dataframe.columns ])
         ),
         html.Tbody([
             html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+                html.Td(dataframe.iloc[ i ][ col ]) for col in dataframe.columns
             ]) for i in range(min(len(dataframe), max_rows))
         ])
     ])
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [ 'https://codepen.io/chriddyp/pen/bWLwgP.css' ]
 
-name =__name__
+name = __name__
 name = "Club Raiders"
 app = dash.Dash(name, external_stylesheets=external_stylesheets)
 
+opts = []
+for it in systems.keys():
+   val: Tuple = systems.get(it)
+   opts.append({'label': it, 'value': str(val)})
+
 app.layout = html.Div([
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=opts,
+        #[
+            #{'label': 'New York City', 'value': 'NYC'},
+            #{'label': 'Montreal', 'value': 'MTL'},
+            #{'label': 'San Francisco', 'value': 'SF'}
+        #],
+        value='NYC'
+    ),
+    html.Div(id='dd-output-container'),
     dash_table.DataTable(
         id='datatable-interactivity',
         columns=[
-            {"name": i, "id": i, "deletable": False, "selectable": False} for i in df.columns
+            {"name": i,
+             "id": i,
+             "deletable": False,
+             "selectable": False}
+            for i in df.columns
         ],
         data=df.to_dict('records'),
         editable=False,
         filter_action="native",
         sort_action="native",
         sort_mode="multi",
-        column_selectable=False, #"single",
-        row_selectable=False, #"multi",
+        column_selectable=False,  # "single",
+        row_selectable=False,  # "multi",
         row_deletable=False,
-        selected_columns=[],
-        selected_rows=[],
+        selected_columns=[ ],
+        selected_rows=[ ],
         page_action="native",
-        page_current= 0,
-        page_size= 100,
+        page_current=0,
+        page_size=100,
     ),
     html.Div(id='datatable-interactivity-container')
 ])
 
+
 @app.callback(
     Output('datatable-interactivity', 'style_data_conditional'),
-    [Input('datatable-interactivity', 'selected_columns')]
+    [ Input('datatable-interactivity', 'selected_columns') ]
 )
 def update_styles(selected_columns):
-    return [{
-        'if': { 'column_id': i },
-        'background_color': colors['background'],  # '#D2F3FF'
-        'font_color': colors['text']
-    } for i in selected_columns]
+    return [ {
+        'if': {'column_id': i},
+        'background_color': colors[ 'background' ],  # '#D2F3FF'
+        'font_color': colors[ 'text' ]
+    } for i in selected_columns ]
+
 
 @app.callback(
     Output('datatable-interactivity-container', "children"),
-    [Input('datatable-interactivity', "derived_virtual_data"),
-     Input('datatable-interactivity', "derived_virtual_selected_rows")])
+    [ Input('datatable-interactivity', "derived_virtual_data"),
+      Input('datatable-interactivity', "derived_virtual_selected_rows") ])
 def update_graphs(rows, derived_virtual_selected_rows):
     # When the table is first rendered, `derived_virtual_data` and
     # `derived_virtual_selected_rows` will be `None`. This is due to an
@@ -96,12 +121,12 @@ def update_graphs(rows, derived_virtual_selected_rows):
     # `derived_virtual_data=df.to_rows('dict')` when you initialize
     # the component.
     if derived_virtual_selected_rows is None:
-        derived_virtual_selected_rows = []
+        derived_virtual_selected_rows = [ ]
 
     dff = df if rows is None else pd.DataFrame(rows)
 
-    colors = ['#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
-              for i in range(len(dff))]
+    colors = [ '#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
+               for i in range(len(dff)) ]
 
     return [
         dcc.Graph(
@@ -109,8 +134,8 @@ def update_graphs(rows, derived_virtual_selected_rows):
             figure={
                 "data": [
                     {
-                        "x": dff["country"],
-                        "y": dff[column],
+                        "x": dff[ "systemName" ],
+                        "y": dff[ column ],
                         "type": "bar",
                         "marker": {"color": colors},
                     }
@@ -129,7 +154,7 @@ def update_graphs(rows, derived_virtual_selected_rows):
         # check if column exists - user may have deleted it
         # If `column.deletable=False`, then you don't
         # need to do this check.
-        for column in ["pop", "lifeExp", "gdpPercap"] if column in dff
+        for column in [ "influence", "population", "gdpPercap" ] if column in dff
     ]
 
 
