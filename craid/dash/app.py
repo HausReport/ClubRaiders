a6786432e19cc999b2f44c64e41769d900e50d60
HausReport <tj_willis@hotmail.com>
2020-05-25 04:00:15 -0400

    incremental

--- craid/dash/app.py
+++ craid/dash/app.py
@@ -15,9 +15,19 @@ import craid.eddb.DataProducer as dp
 arrs = dp.getDataArrays()
 csa = arrs[0]
 systems: Dict[str, Tuple[float, float, float]] = arrs[2]
+pFactions: Dict[str, Tuple[float, float, float]] = arrs[1]
 df = dp.getDataFrame(csa)
 
 nrows = df.shape[0]
+
+#
+# load a markdown file from /data
+#
+def getMarkdown(which: str) -> dcc.Markdown:
+    with open("text/"+which+".md", "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+        return dcc.Markdown(text)
+
 
 # suppress_callback_exceptions=True
 
@@ -46,11 +56,13 @@ for it in systems.keys():
     val: Tuple = systems.get(it)
     opts.append({'label': it, 'value': str(it) + "," + str(val[0]) + "," + str(val[1]) + "," + str(val[2])})
 
-# print( df.columns )
+fopts = []
+for it in pFactions.keys():
+    val: Tuple = systems.get(it)
+    if( val != None):
+        opts.append({'label': it, 'value': str(it) + "," + str(val[0]) + "," + str(val[1]) + "," + str(val[2])})
 
-datatable: dash_table.DataTable = dash_table.DataTable(
-    id='datatable-interactivity',
-    columns=[
+theColumns = [
         {"name": 'systemName', "id": 'systemName', "deletable": False, "selectable": False},
         {"name": 'factionName', "id": 'factionName', "deletable": False, "selectable": False},
         {"name": 'x', "id": 'x', "deletable": False, "selectable": False, "hideable": True, "type": "numeric"},
@@ -63,7 +75,11 @@ datatable: dash_table.DataTable = dash_table.DataTable(
         {"name": 'control', "id": 'control', "deletable": False, "selectable": False},
         {"name": 'vulnerable', "id": 'vulnerable', "deletable": False, "selectable": False},
         {"name": 'distance', "id": 'distance', "deletable": False, "selectable": False, "type": "numeric"},
-    ],
+    ]
+datatable: dash_table.DataTable = dash_table.DataTable(
+    id='datatable-interactivity',
+    columns= theColumns,
+    #hidden_columns = {'x','y','z'},   causes an exception re serialization
     data=df.to_dict('records'),
     editable=False,
     filter_action="native",
@@ -81,7 +97,9 @@ datatable: dash_table.DataTable = dash_table.DataTable(
 
 # tput("datatable-interactivity", "filtering_settings"),
 # tab.available_properties['filter_query'] = "{control} contains false && {influence} < 25"
-
+datatable.filter_query = "{isHomeSystem} contains false && {influence} < 25"
+#datatable.filter_query = "{isHomeSystem} contains false && {vulnerable} contains War"
+#datatable.hidden_columns = {'x','y','z'}
 # for x1 in tab.available_properties:
 # print( str(x1) )
 
@@ -94,12 +112,18 @@ hdr_layout = html.Div([
         value='Alioth',
         placeholder='Select star system'
     ),
+    dcc.Dropdown(
+        id='demo-dropdown2',
+        options=fopts,
+        value='Anti Xeno Initiative',
+        placeholder='Select player faction'
+    ),
+
     html.Div(id='dd-output-container')
 ], style={'width': '99%', 'display': 'inline-block'})
 
 tab1_layout = html.Div([
-    datatable,
-    html.Div(id='datatable-interactivity-container')
+    getMarkdown("bh")
 ])
 
 # =============================================================
@@ -109,22 +133,16 @@ app.layout = html.Div([
     hdr_layout,
     dcc.Tabs(id='tabs-example', value='tab-1', children=[
         dcc.Tab(label='Overview', value='tab-1'),
-        dcc.Tab(label='Bounty Hunting', value='tab-2'),
-        dcc.Tab(label='Combat Zones', value='tab-3'),
-        dcc.Tab(label='Trade/Exploration', value='tab-4'),
-        dcc.Tab(label='Missions', value='tab-5'),
+        dcc.Tab(label='Combat Zones', value='tab-2'),
+        dcc.Tab(label='Bounty Hunting', value='tab-3'),
+        dcc.Tab(label='Trade/Exploration/Missions', value='tab-4'),
+        dcc.Tab(label='Scouting', value='tab-5'),
     ]),
-    html.Div(id='tabs-example-content')
+    html.Div(id='tabs-example-content'),
+    datatable,
+    html.Div(id='datatable-interactivity-container')
 ])
 
-
-#
-# load a markdown file from /data
-#
-def getCZMarkdown():
-    with open("../data/cz.md", "r", encoding="utf-8") as input_file:
-        text = input_file.read()
-        return dcc.Markdown(text)
 
 
 @app.callback(Output('tabs-example-content', 'children'),
@@ -137,7 +155,7 @@ def render_content(tab):
         # ])
     elif tab == 'tab-2':
         return html.Div([
-            getCZMarkdown()
+            getMarkdown("cz")
         ])
     elif tab == 'tab-3':
         return html.Div([
@@ -164,13 +182,11 @@ def render_content(tab):
 # =============================================================
 # Callback handlers below
 # =============================================================
-
 @app.callback(
     [dash.dependencies.Output('datatable-interactivity', 'data'),
     dash.dependencies.Output('datatable-interactivity', 'columns')],
     [dash.dependencies.Input('demo-dropdown', 'value')])
 def update_output(value):
-    _cols: List[Dict[str, str]] = [{"name": 'distance', "id": 'distance'}]
     x, y, z = 0.0, 0.0, 0.0
     try:
         spl = value.split(",")
@@ -181,7 +197,6 @@ def update_output(value):
         print("woops")
 
     for ind in df.index:
-        # print(df['Name'][ind], df['Stream'][ind])
         x1: float = df.at[ind, 'x']
         y1: float = df.at[ind, 'y']
         z1: float = df.at[ind, 'z']
@@ -189,48 +204,11 @@ def update_output(value):
         df.at[ind, 'distance'] = dis
 
     print(df['distance'].dtypes)
-    _cols = [
-        {"name": i, "id": i} for i in df.columns
-    ]
+    _cols = theColumns
+    # [
+    #     {"name": i, "id": i} for i in df.columns
+    # ]
     return df.to_dict('records'), _cols
-
-
-# @app.callback(
-#     [dash.dependencies.Output('datatable-interactivity', 'data'),
-#      dash.dependencies.Output('datatable-interactivity', 'columns')],
-#     [dash.dependencies.Input('demo-dropdown', 'value')])
-# def update_output(value):
-#     nrows = df.shape[0]
-#     #ret = df['distance'] #'[1] * nrows
-#     columns = [{"name": 'distance', "id": 'distance'}]
-#     #tab['data']
-#     x = 0.0
-#     y = 0.0
-#     z = 0.0
-#     try:
-#         spl = value.split(",")
-#         x = float(spl[1])
-#         y = float(spl[2])
-#         z = float(spl[3])
-#     except:
-#         print("woops")
-#         #print( str(x) + str(y) + str(z))
-#
-#     for i in range(0, nrows):
-#         x1= df.at[i,'x']
-#         dis = math.sqrt( (x-x1)**2)
-#         df.at[i,'distance'] = dis
-#         #ret[i]= str(dis)
-#         #ret = df['distance']  # '[1] * nrows
-#     print(dis)
-#     #for row in df.itertuples():
-#         #val: float = df.get(row.Index,'distance')
-#         #dis = abs( x-val)
-#         #df.set_value(row.Index, dis, 'distance' )
-#     #for i in range(0, nrows):
-#         #df['distance'] = df
-#     #return [ret.to_list(), columns]
-#     return [df['distance'].to_dict(), columns]
 
 @app.callback(
     dash.dependencies.Output('dd-output-container', 'children'),
@@ -240,21 +218,26 @@ def update_output(value):
 
 
 # @app.callback(
-# Output('datatable-interactivity', 'style_data_conditional'),
-# [Input('datatable-interactivity', 'selected_columns')] )
-# def update_styles(selected_columns):
-# return [{
-# 'if': {'column_id': i},
-# 'background_color': colors['background'],  # '#D2F3FF'
-# 'font_color': colors['text']
-# } for i in selected_columns]
+#     dash.dependencies.Output('dd-output-container', 'children'),
+#     [Input('datatable-row-ids', 'active_cell')])
+# def update_graphs(row_ids, active_cell):
+#     if row_ids is None:
+#         dff = df
+#         # pandas Series works enough like a list for this to be OK
+#         row_ids = df['id']
+#     else:
+#         dff = df.loc[row_ids]
+#
+#     active_row_id = active_cell['row_id'] if active_cell else None
+#     return "You selected row " + active_row_id
 
 
 @app.callback(
     Output('datatable-interactivity-container', "children"),
     [Input('datatable-interactivity', "derived_virtual_data"),
-    Input('datatable-interactivity', "derived_virtual_selected_rows")])
-def update_graphs(rows, derived_virtual_selected_rows):
+    Input('datatable-interactivity', "derived_virtual_selected_rows"),
+    Input('datatable-interactivity', 'active_cell')])
+def update_graphs(rows, derived_virtual_selected_rows, active_cell):
     # When the table is first rendered, `derived_virtual_data` and
     # `derived_virtual_selected_rows` will be `None`. This is due to an
     # idiosyncracy in Dash (unsupplied properties are always None and Dash
@@ -272,6 +255,12 @@ def update_graphs(rows, derived_virtual_selected_rows):
     colors = ['#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
                for i in range(len(dff))]
 
+    if active_cell:
+        print("You selected row " + str(active_cell))
+    #active_row_id = active_cell['row_id'] if active_cell else None
+    #if( active_row_id != None):
+        #print("You selected row " + active_row_id)
+    #print("You selected row " + dff["systemName"][0])
     return [
         dcc.Graph(
             id=column,
