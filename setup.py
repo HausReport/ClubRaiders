7cbc2e0dee8d69fc4a741c560aaac543dd2c2573
HausReport <tj_willis@hotmail.com>
2020-05-26 06:44:09 -0400

    structure+io tweaks

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
     author_email='Erlaed2@fdev.not',
     # Needed to actually package something
     # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    packages=['craid', 'craid.eddb', 'craid.club', 'craid.css', 'craid.dash', 'craid.dash.assets','craid.dash.queries',
-              'craid.dash.text', 'craid.eddb', 'craid.club.regions'],
+    packages=['craid', 'craid.eddb', 'craid.club', 'craid.css', 'craid.dashbd', 'craid.dashbd.assets','craid.dashbd.queries',
+              'craid.dashbd.text', 'craid.eddb', 'craid.club.regions'],
     # Needed for dependencies
     install_requires=['numpy', 'dash', 'pandas', 'requests', 'setuptools'],
     # *strongly* suggested for sharing
-    version='0.57',
+    version='0.58',
     # The license can be anything you like
     license='MIT',
     description='Placeholder description',
