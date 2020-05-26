ab9db98e3235fadf3ff41bc776dd2ff3441075b3
HausReport <tj_willis@hotmail.com>
2020-05-26 06:27:58 -0400

    structure/packaging

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     author_email='Erlaed2@fdev.not',
     # Needed to actually package something
     # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    packages=['craid', 'craid.eddb', 'craid.club', 'craid.css', 'craid.dash', 'craid.dash.queries', 'craid.dash.text',
-              'craid.eddb', 'craid.club.regions'],
+    packages=['craid', 'craid.eddb', 'craid.club', 'craid.css', 'craid.dash', 'craid.dash.assets','craid.dash.queries',
+              'craid.dash.text', 'craid.eddb', 'craid.club.regions'],
     # Needed for dependencies
     install_requires=['numpy', 'dash', 'pandas', 'requests', 'setuptools'],
     # *strongly* suggested for sharing
