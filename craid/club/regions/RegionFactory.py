e0ae252b21f673a9c7159ab9f8b777ba5eb4f777
HausReport <tj_willis@hotmail.com>
2020-06-20 06:52:42 -0400

    working on regions

--- craid/club/regions/RegionFactory.py
+++ craid/club/regions/RegionFactory.py
@@ -17,13 +17,16 @@ from craid.eddb.SquadronXYZ import SquadronXYZ
 
 class RegionFactory(object):
     regionDict = {
-        1: SphericalRegion("Merope", 1, -79, -150, -340, 100, 'rgb(255, 0, 0)'),
-        2: SphericalRegion("Rectangle", 2, -54, -66, -124, 75, 'rgb(255,255,0)'),
-        3: SphericalRegion("California", 3, -320, -217, -913, 200, 'rgb(32,32,32)'),
-        4: SphericalRegion("Sirius", 4, 43, -3, 78, 125, 'rgb(0, 255, 0)'),
-        5: SphericalRegion("Xi Shan", 5, 155, 84, -50, 45, 'rgb(255,0,255)'),
-        6: SphericalRegion("Abroin", 6, -94, 110, -40, 30, 'rgb(0,255,255)'),
-        7: SphericalRegion("Hip 51652", 7, 27, 151, -81, 35, 'rgb(0,0,255)'),
+        1: SphericalRegion("Merope",        1,  -79,  -150, -340, 100, 'rgb(255, 0, 0)'),
+        2: SphericalRegion("Rectangle",     2,  -54,   -66, -124,  75, 'rgb(255,255,0)'),
+        3: SphericalRegion("California",    3, -320,  -217, -913, 200, 'rgb(32,32,32)'),
+        4: SphericalRegion("Sirius",        4,    6,    -1,   -6,  35, 'rgb(0, 255, 0)'),
+        5: SphericalRegion("Xi Shan",       5,  155,    84,  -50,  45, 'rgb(255,0,255)'),
+        6: SphericalRegion("Abroin",        6,  -94,   110,  -40,  30, 'rgb(0,255,255)'),
+        7: SphericalRegion("Bentonia",      7,   27,   151,  -81,  35, 'rgb(0,0,255)'),
+        8: SphericalRegion("Hodack",        8,   60,    23,   46,  35, 'rgb(0,0,255)'),
+        9: SphericalRegion("Wreaken",       9,   32,    18,  114,  25, 'rgb(0,0,255)'),
+       10: SphericalRegion("CQC",          10,   18,   -11,   78,  30, 'rgb(0,0,255)'),
     }
     unRegion = TheUnregion()
 
@@ -35,6 +38,14 @@ class RegionFactory(object):
                 return reg
 
         return RegionFactory.unRegion
+
+    @staticmethod
+    def getRegionId(x:int) -> SphericalRegion:
+        reg = RegionFactory.regionDict.get(x)
+        if reg is None:
+            return RegionFactory.unRegion
+
+        return reg
 
     # @staticmethod
     # def getRegion(sys: craid.eddb.System.System) -> SphericalRegion:
