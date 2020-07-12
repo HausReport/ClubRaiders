13fcdf3673ba559061a596d3db90ea63de76de85
HausReport <tj_willis@hotmail.com>
2020-07-12 05:20:58 -0400

    daily data+moving fwd on ann data

--- craid/history/Net.py
+++ craid/history/Net.py
@@ -3,6 +3,7 @@
 #
 #   SPDX-License-Identifier: BSD-3-Clause
 import os
+import shutil
 from pprint import pprint
 from typing import Dict, List
 
@@ -25,33 +26,48 @@ print(f"Output file: {outfile}.")
 #fName = loader.find_data_file("jul8.gz")
 #print(fName)
 
-prod = getDataArrays(loader= loader, clubSystemsOnly=False)
+datafiles = [ 'jul4.gz','jul5.gz','jul6.gz','jul7.gz','jul8.gz','jul9.gz','jul10.gz', 'jul11.gz']
 
-theDict = prod['sysIdFacIdToFactionInstance']
+for short in datafiles:
+    fromFile = os.path.join('D:','systems',short)
+    toFile = os.path.join('D:','systems','systems_populated.jsonl.gz')
+    shutil.copyfile(fromFile,toFile)
 
-print("Reading bigfile")
-#base: pd.DataFrame = pd.DataFrame()
-base = pd.read_json(outfile)
-foo: List[Dict] = []
+    prod = getDataArrays(loader= loader, clubSystemsOnly=False)
 
-siz = len(theDict)
-print(f"Total size: {siz}")
-i = 0
-for facInst in theDict.values():
-    i += 1
-    if i%50 == 0:
-        print(".", end='')
-    if i%(50*100)==0:
-        print(f"\nRow:{i}",end='')
-    # if i>250:
-    #     break
-    new_row = facInst.getANNRow()
-    #df = df.append(new_row, ignore_index=True)
-    foo.append(new_row)
-    #pprint(facInst.getANNRow())
+    theDict = prod['sysIdFacIdToFactionInstance']
 
-base = base.append(foo, ignore_index=True)
-base.to_json(outfile)
+    if os.path.exists(outfile):
+        print("Reading bigfile")
+        base = pd.read_json(outfile)
+    else:
+        print("No bigfile - creating a new one")
+        base: pd.DataFrame = pd.DataFrame()
+
+    foo: List[Dict] = []
+
+    siz = len(theDict)
+    print(f"Total size: {siz}")
+    i = 0
+    for facInst in theDict.values():
+        i += 1
+        if i%50 == 0:
+            print(".", end='')
+        if i%(50*100)==0:
+            print(f"\nRow:{i}",end='')
+        # if i>250:
+        #     break
+        new_row = facInst.getANNRow()
+        #df = df.append(new_row, ignore_index=True)
+        foo.append(new_row)
+        #pprint(facInst.getANNRow())
+
+    base = base.append(foo, ignore_index=True)
+    # can't fix the dates yet
+    #base['updated'] = pd.to_datetime(base['updated'])
+    #base['updated'] = base.updated.dt.round("D")
+    base = base.drop_duplicates()
+    base.to_json(outfile)
+
 beep()
-
 #print(df)
--- craid/history/NetFix.py
+++ craid/history/NetFix.py
@@ -3,12 +3,20 @@
 #
 #   SPDX-License-Identifier: BSD-3-Clause
 import os
+from typing import List, Dict
 import pandas as pd
+import winsound
+
+def beep():
+    duration = 2000  # milliseconds
+    freq = 440  # Hz
+    winsound.Beep(freq, duration)
 
 def printShape(df):
-    print( str(df.shape))
+    print(str(df.shape))
 
-infile = os.path.join('D:','systems','ann2.jsonl')
+
+infile = os.path.join('D:', 'systems', 'ann.jsonl')
 print(f"Input file: {infile}.")
 
 base = pd.read_json(infile)
@@ -23,42 +31,52 @@ printShape(base)
 # prepare data
 df = base
 
-df['updated']= pd.to_datetime(df['updated'])
+df['updated'] = pd.to_datetime(df['updated'])
 df['updated'] = df.updated.dt.round("D")
-df = df.sort_values(by=['sysid', 'facid','updated'])
+df = df.drop_duplicates()
+df = df.sort_values(by=['sysid', 'facid', 'updated'])
 
 # this drops out all data that doesn't have at least 2 days in a row
 systems = df['sysid'].unique().tolist()
-holder = pd.DataFrame()
+
+# try with holder as a List[Dict]
+# holder = pd.DataFrame()
+foo: List[Dict] = []
 
 sysLen = len(systems)
 i = 0
 for sys in systems:
-  i = i + 1
-  perc = (i*100.0)/sysLen
-  print(f"{sys} - {perc}")
-  sysSlice = df[ df['sysid'] ==sys]
-  facs = sysSlice['facid'].unique().tolist()
-  for fac in facs:
-    print(f"\t {fac}")
-    facSlice = sysSlice[ sysSlice['facid'] == fac].reset_index()
-    facSlice['diff1'] = facSlice.updated.diff(periods=1).dt.days.abs()
-    facSlice['diff2'] = facSlice.updated.diff(periods=-1).dt.days.abs()
-    facSlice = facSlice.fillna(999.0)
-    facSlice['diffz'] = facSlice[['diff1','diff2']].min(axis=1)
-    facSlice = facSlice[ facSlice['diffz'] <=1.0]
-    facSlice = facSlice.drop(['diff1','diff2','diffz'],  axis=1)
-    holder = holder.append(facSlice.copy(), ignore_index=True)
+    i = i + 1
+    percentDone = (i * 100.0) / sysLen
+    print(f"{sys} - {percentDone}")
+    sysSlice = df[df['sysid'] == sys]
+    factions = sysSlice['facid'].unique().tolist()
+    for fac in factions:
+        #print(f"\t {fac}")
+        facSlice = sysSlice[sysSlice['facid'] == fac].reset_index()
+        facSlice['diff1'] = facSlice.updated.diff(periods=1).dt.days.abs()
+        facSlice['diff2'] = facSlice.updated.diff(periods=-1).dt.days.abs()
+        facSlice = facSlice.fillna(999.0)
+        facSlice['diffz'] = facSlice[['diff1', 'diff2']].min(axis=1)
+        facSlice = facSlice[facSlice['diffz'] <= 1.0]
+        facSlice = facSlice.drop(['diff1', 'diff2', 'diffz'], axis=1)
 
+        aDict = facSlice.to_dict('records')
+        # holder = holder.append(facSlice.copy(), ignore_index=True)
+        foo.append(aDict)
+
+print("Converting from list of dict to dataframe")
+holder = pd.DataFrame()
+holder = holder.append(foo, ignore_index=True)
 
 printShape(holder)
 
-
-
 #
 #
 #
-outfile = os.path.join('D:','systems','holder.jsonl')
+outfile = os.path.join('D:', 'systems', 'holder2.jsonl')
 print(f"Output file: {outfile}.")
 holder.to_json(outfile)
-
+print("Save complete.")
+beep()
+print("Run complete.")
