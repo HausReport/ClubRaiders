ec2f825ccce625252bd914c39b78cec79fcb907c
HausReport <tj_willis@hotmail.com>
2020-07-21 03:50:18 -0400

    playing with multiprocess lock for data crunching

--- craid/eddb/loader/DataProducer.py
+++ craid/eddb/loader/DataProducer.py
@@ -5,16 +5,12 @@
 import gc
 import logging
 from typing import Dict, Set
-
+import multiprocessing
 import pandas as pd
-
 import craid.eddb.util.Printmem as pm
 from craid.eddb.base.Aware import Aware
 from craid.eddb.faction.Faction import Faction
 from craid.eddb.loader.CreateClubSystemKeys import getClubSystemKeys
-#
-# Expensive function - run once, use result many times
-#
 from craid.eddb.loader.CreateDataFrame import getDataFrame
 from craid.eddb.loader.CreateFactionInstances import getFactionInstances
 from craid.eddb.loader.CreateFactions import load_factions
@@ -24,105 +20,114 @@ from craid.eddb.loader.MakeKeyFiles import dumpKeys, loadKeys
 from craid.eddb.loader.strategy.AWSLoader import LoadDataFromAWS
 from craid.eddb.loader.strategy.EDDBLoader import LoadDataFromEDDB
 
+lock = multiprocessing.Lock()
+
 
 def getDataArrays(writeKeyFiles=False, useEddb=False, loader=None, clubSystemsOnly=True) -> Dict[str, object]:
-
-    if loader is not None:
-        logging.info("Using supplied loader")
-        myLoader = loader
-    else:
-        if useEddb:
-            logging.info("Loading from EDDB")
-            myLoader = LoadDataFromEDDB()
+    global lock
+    #lock = multiprocessing.Lock()
+    #lock.acquire()
+    with lock:
+        logging.info("Acquired lock.")
+        if loader is not None:
+            logging.info("Using supplied loader")
+            myLoader = loader
         else:
-            logging.info("Loading from AWS")
-            myLoader = LoadDataFromAWS()  # LoadDataFromGithub()
+            if useEddb:
+                logging.info("Loading from EDDB")
+                myLoader = LoadDataFromEDDB()
+            else:
+                logging.info("Loading from AWS")
+                myLoader = LoadDataFromAWS()  # LoadDataFromGithub()
 
-    playerFactionNameToSystemName: Dict[str, str] = {}
+        playerFactionNameToSystemName: Dict[str, str] = {}
 
-    pm.printmem('0')
-    #
-    # Load the basic factions and systems structures
-    #
-    all_factions_dict, player_faction_keys, club_faction_keys = load_factions(myLoader)
-    pm.printmem('0.5')
-    all_systems_dict = load_systems(myLoader)
+        pm.printmem('0')
+        #
+        # Load the basic factions and systems structures
+        #
+        all_factions_dict, player_faction_keys, club_faction_keys = load_factions(myLoader)
+        pm.printmem('0.5')
+        all_systems_dict = load_systems(myLoader)
 
-    gc.collect()
-    pm.printmem('1')
+        gc.collect()
+        pm.printmem('1')
 
-    #
-    # Populate dict of player faction name -> system name
-    # Used by dropdowns in dashboard
-    #
-    k: int
-    for k in player_faction_keys:
-        fac: Faction = all_factions_dict[k]
-        fac_name: str = fac.get_name()
-        sys_id: int = all_factions_dict[k].homesystem_id
-        sys = all_systems_dict.get(sys_id)
-        if sys is not None:
-            sys_name: str = sys.get_name()
-            playerFactionNameToSystemName[fac_name] = sys_name
+        #
+        # Populate dict of player faction name -> system name
+        # Used by dropdowns in dashboard
+        #
+        k: int
+        for k in player_faction_keys:
+            fac: Faction = all_factions_dict[k]
+            fac_name: str = fac.get_name()
+            sys_id: int = all_factions_dict[k].homesystem_id
+            sys = all_systems_dict.get(sys_id)
+            if sys is not None:
+                sys_name: str = sys.get_name()
+                playerFactionNameToSystemName[fac_name] = sys_name
 
-    #
-    # Identify systems with club faction presence
-    #
-    clubSystemKeysExists = True
-    club_system_keys = loadKeys('club-system-keys')
-    if club_system_keys is None:
-        club_system_keys = getClubSystemKeys(all_systems_dict, club_faction_keys)
-        clubSystemKeysExists = False
+        #
+        # Identify systems with club faction presence
+        #
+        clubSystemKeysExists = True
+        club_system_keys = loadKeys('club-system-keys')
+        if club_system_keys is None:
+            club_system_keys = getClubSystemKeys(all_systems_dict, club_faction_keys)
+            clubSystemKeysExists = False
 
-    #
-    # Make (2?) nifty list(s) of club faction presences
-    #
-    allClubSystemInstances, sysIdFacIdToFactionInstance, factions_of_interest_keys \
-        = getFactionInstances(all_systems_dict, club_system_keys, all_factions_dict, club_faction_keys, clubSystemsOnly=clubSystemsOnly)
+        #
+        # Make (2?) nifty list(s) of club faction presences
+        #
+        allClubSystemInstances, sysIdFacIdToFactionInstance, factions_of_interest_keys \
+            = getFactionInstances(all_systems_dict, club_system_keys, all_factions_dict, club_faction_keys, clubSystemsOnly=clubSystemsOnly)
 
-    gc.collect()
-    pm.printmem('2')
+        gc.collect()
+        pm.printmem('2')
 
-    #
-    # Give global faction info to systems and
-    # give global system info to factions
-    #
-    Aware.setSystemsDict(all_systems_dict)
-    Aware.setFactionsDict(all_factions_dict)
+        #
+        # Give global faction info to systems and
+        # give global system info to factions
+        #
+        Aware.setSystemsDict(all_systems_dict)
+        Aware.setFactionsDict(all_factions_dict)
 
-    #
-    # Only now, can we populate lists of stations in **club** systems
-    # No return value - stations are stored in their respective system objects
-    #
-    club_station_keys: Set[int] = \
-        loadStationsInClubSystems(myLoader, all_systems_dict, club_faction_keys, club_system_keys)
+        #
+        # Only now, can we populate lists of stations in **club** systems
+        # No return value - stations are stored in their respective system objects
+        #
+        club_station_keys: Set[int] = \
+            loadStationsInClubSystems(myLoader, all_systems_dict, club_faction_keys, club_system_keys)
 
-    gc.collect()
-    pm.printmem('3')
-    #
-    # And, finally return the big honking dict of things
-    #
+        gc.collect()
+        pm.printmem('3')
+        #
+        # And, finally return the big honking dict of things
+        #
 
-    df: pd.DataFrame = getDataFrame(allClubSystemInstances)
+        df: pd.DataFrame = getDataFrame(allClubSystemInstances)
 
-    #
-    # Clean up some resources
-    #
+        #
+        # Clean up some resources
+        #
 
-    if writeKeyFiles:
-        dumpKeys("club-system-keys", club_system_keys)
-        dumpKeys("factions-of-interest-keys", factions_of_interest_keys)
-        dumpKeys("club-station-keys", club_station_keys)
+        if writeKeyFiles:
+            dumpKeys("club-system-keys", club_system_keys)
+            dumpKeys("factions-of-interest-keys", factions_of_interest_keys)
+            dumpKeys("club-station-keys", club_station_keys)
 
-    # FIXME - think about this
-    # if not factions_of_interest_keys:
-    # if not clubSystemKeysExists:
+        # FIXME - think about this
+        # if not factions_of_interest_keys:
+        # if not clubSystemKeysExists:
 
-    allClubSystemInstances.clear()
-    allClubSystemInstances = None
-    club_faction_keys.clear()
-    club_faction_keys = None
-    gc.collect()
+        allClubSystemInstances.clear()
+        allClubSystemInstances = None
+        club_faction_keys.clear()
+        club_faction_keys = None
+        gc.collect()
+
+    #    lock.release() # FIXME: needs to be in a finally: block
+    logging.info("Released lock.")
 
     # 'playerFactionIdToInfo': playerFactionIdToInfo,
     #
