39435596ca73631bb2b8cea3a1105223de02253b
HausReport <tj_willis@hotmail.com>
2020-12-06 08:54:22 -0500

    add test

---
+++ craid/bgsBuddy/test/test_DailyPlan.py
@@ -0,0 +1,27 @@
+#   Copyright (c) 2020 Club Raiders Project
+#   https://github.com/HausReport/ClubRaiders
+#
+#   SPDX-License-Identifier: BSD-3-Clause
+from pprint import pprint
+from unittest import TestCase
+
+from craid.bgsBuddy import GlobalDictionaries
+from craid.bgsBuddy.helpers.DailyPlan import DailyPlan
+
+
+class TestDailyPlan(TestCase):
+    def setUp(self):
+        GlobalDictionaries.init_logger()
+        self.samplePlan: DailyPlan = DailyPlan("LHS 2477", "Federal Reclamation Co", "Hodack Prison Colony")
+        self.samplePlan.addMissionInfluenceGoal(60)
+        self.samplePlan.addBountyGoal(16000000)
+        self.samplePlan.addCartographyGoal(8000000)
+        self.samplePlan.addTradeProfitGoal(16000000)
+
+
+    def tearDown(self):
+        dv = self.samplePlan.to_dict()
+        pprint(dv)
+
+    def test_to_dict(self):
+        pass
